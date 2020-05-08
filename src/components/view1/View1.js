75cb579ff2231d1830fb837e0c5fc7e4f204d1d6
Anand450623 <ak450623@gmail.com>
2020-05-08 23:52:42 +0530

    change test

--- src/components/view1/View1.js
+++ src/components/view1/View1.js
@@ -5,7 +5,7 @@ class View1 extends React.Component
     render() 
     {
         return <React.Fragment>
-            <h1>Branch </h1>
+            <h1>Branch Dynamic Vu-1</h1>
             <h2> Inside View1</h2>
             </React.Fragment>
     }
