7182203d608a87106bc7919b6045d675200eff45
Anand450623 <ak450623@gmail.com>
2020-05-14 19:13:19 +0530

    look n feel

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import view1 from './view1/View1.js';
 import view2 from './view2/View2.js';
 import view3 from './view3/View3.js';
 import view4 from './view4/View4.js';
+import './../css/App.css';
 
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
@@ -13,17 +14,19 @@ class App extends React.Component
     {
         return (<Router>
                     <div style={{marginBottom:"2%",backgroundColor:"lightgray"}} >
-                        <h1>Indian Banking Analytics</h1>
-                        <Link to="/view1">View1</Link> |
-                        <Link to="/view2"> View2 </Link> |
-                        <Link to="/view3"> View3 </Link> |
-                        <Link to="/view4"> View4 </Link>
-                        <br/> <br/>
-                        <Route exact path = "/" component = {welcome} />
-                        <Route path = "/view1" component = {view1} />
-                        <Route path = "/view2" component = {view2} />
-                        <Route path = "/view3" component = {view3} />
-                        <Route path = "/view4" component = {view4} />
+                        <h1 id="heading">Indian Banking Analytics</h1>
+                        <div id="appcontent">
+                            <Link to="/view1" className="btn">View1</Link> 
+                            <Link to="/view2" className="btn"> View2 </Link> 
+                            <Link to="/view3" className="btn"> View3 </Link> 
+                            <Link to="/view4" className="btn"> View4 </Link>
+                            <br/> <br/>
+                            <Route exact path = "/" component = {welcome} />
+                            <Route path = "/view1" component = {view1} />
+                            <Route path = "/view2" component = {view2} />
+                            <Route path = "/view3" component = {view3} />
+                            <Route path = "/view4" component = {view4} />
+                        </div>
                     </div>
                 </Router>)
     }
